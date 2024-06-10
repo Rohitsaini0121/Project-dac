4d07528d717312fcc71e1ce2b817d89f8fc63946
Rohitsaini0121 <86484897+Rohitsaini0121@users.noreply.github.com>
2024-06-10 11:21:59 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,23 @@
+package day1;
+//wap to take two numbers and print their sum 
+
+public class Addition {
+
+	int k;
+	
+	public static void main(String[] args) {
+
+//     int  sal=32768;
+//     float r=11.2f;//without 'f' it will give compile time error
+//     double t=22.33;//by default decimal in code is double 
+//     char c='a';// 2 byte - unicode 
+//     String s="sneha";//String is a class 
+		
+		int a=10;
+		int b=20;
+		System.out.println(" sum ="+ (a + b) );
+
+	}
+
+}
+
